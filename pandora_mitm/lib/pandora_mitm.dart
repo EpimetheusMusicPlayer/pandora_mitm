c47c038e8b68c63e654c90670cf25e98d87d9bf8
hacker1024 <hacker1024@users.sourceforge.net>
2022-06-18 16:20:45 +1000

    feat: Export request and response classes

--- pandora_mitm/lib/pandora_mitm.dart
+++ pandora_mitm/lib/pandora_mitm.dart
@@ -1,4 +1,8 @@
+export 'package:iapetus/iapetus_data.dart' show PandoraApiResponse;
+
+export 'src/entities/api_request.dart';
 export 'src/entities/modification_record.dart';
+export 'src/entities/pandora_response.dart';
 export 'src/entities/record.dart';
 export 'src/pandora_mitm.dart';
 export 'src/pandora_mitm_plugin.dart';
