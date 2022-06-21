3c2913e664a5f754e296e1cf851b177fca6719f1
hacker1024 <hacker1024@users.sourceforge.net>
2022-06-22 00:08:12 +1000

    feat(gui): Send CheckedFromJsonException exceptions into the object stream

--- pandora_mitm_gui_core/lib/src/plugins/mixins/object_stream.dart
+++ pandora_mitm_gui_core/lib/src/plugins/mixins/object_stream.dart
@@ -30,47 +30,51 @@ mixin ObjectStreamMixin on pmplg.StreamPlugin {
 
   Object? _extractObject(String apiMethod, Object? response) {
     if (response is! Map<String, dynamic>) return null;
-    switch (apiMethod) {
-      case 'catalog.v4.annotateObjects':
-      case 'playlists.v7.annotatePlaylists':
-        return response.map(
-          (pandoraId, annotationJson) => MapEntry(
-            pandoraId,
-            MediaAnnotation.fromJson(annotationJson as Map<String, dynamic>),
-          ),
-        );
-      case 'catalog.v4.getDetails':
-        return null;
-      case 'contentservice.getcontent':
-        return StationContentSet.fromJson(response);
-      case 'station.getPlaylist':
-        return (response['items'] as List<dynamic>)
-            .cast<Map<String, dynamic>>()
-            .map(
-              (stationContentJson) =>
-                  StationContent.fromJson(stationContentJson),
-            )
-            .toList(growable: false);
-      case 'collections.v7.getItems':
-        return CollectionListSegment.fromJson(response);
-      case 'pods.v5.getAutoplaySongs':
-        return SongRecommendationSet.fromJson(response);
-      case 'playlists.v7.getTracks':
-        return PlaylistSegment.fromJson(response);
-      case 'onDemand.getAudioPlaybackInfo':
-        return OnDemandMedia.fromJson(response);
-      case 'feed.v1.getDirectory':
-        return DirectoryResponse.fromJson(response);
-      case 'user.getStationList':
-        return StationList.fromJson(response);
-      case 'auth.partnerLogin':
-        return AuthenticatedPartner.fromJson(response);
-      case 'auth.userLogin':
-        return AuthenticatedUser.fromJson(response);
-      case 'user.createUser':
-        return AuthenticatedUser.fromJson(response);
-      default:
-        return null;
+    try {
+      switch (apiMethod) {
+        case 'catalog.v4.annotateObjects':
+        case 'playlists.v7.annotatePlaylists':
+          return response.map(
+            (pandoraId, annotationJson) => MapEntry(
+              pandoraId,
+              MediaAnnotation.fromJson(annotationJson as Map<String, dynamic>),
+            ),
+          );
+        case 'catalog.v4.getDetails':
+          return null;
+        case 'contentservice.getcontent':
+          return StationContentSet.fromJson(response);
+        case 'station.getPlaylist':
+          return (response['items'] as List<dynamic>)
+              .cast<Map<String, dynamic>>()
+              .map(
+                (stationContentJson) =>
+                    StationContent.fromJson(stationContentJson),
+              )
+              .toList(growable: false);
+        case 'collections.v7.getItems':
+          return CollectionListSegment.fromJson(response);
+        case 'pods.v5.getAutoplaySongs':
+          return SongRecommendationSet.fromJson(response);
+        case 'playlists.v7.getTracks':
+          return PlaylistSegment.fromJson(response);
+        case 'onDemand.getAudioPlaybackInfo':
+          return OnDemandMedia.fromJson(response);
+        case 'feed.v1.getDirectory':
+          return DirectoryResponse.fromJson(response);
+        case 'user.getStationList':
+          return StationList.fromJson(response);
+        case 'auth.partnerLogin':
+          return AuthenticatedPartner.fromJson(response);
+        case 'auth.userLogin':
+          return AuthenticatedUser.fromJson(response);
+        case 'user.createUser':
+          return AuthenticatedUser.fromJson(response);
+        default:
+          return null;
+      }
+    } on CheckedFromJsonException catch (e) {
+      return e;
     }
   }
 }
