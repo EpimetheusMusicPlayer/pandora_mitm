b19949905863def3111846cdccb532b57c269739
hacker1024 <hacker1024@users.sourceforge.net>
2022-07-04 00:09:16 +1000

    feat(pandora_mitm): Log HTTP interception backend connection failures

--- pandora_mitm/lib/src/pandora_mitm_logger.dart
+++ pandora_mitm/lib/src/pandora_mitm_logger.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:logging/logging.dart';
 import 'package:meta/meta.dart';
 import 'package:pandora_mitm/src/pandora_mitm_backend.dart';
@@ -25,7 +27,14 @@ mixin PandoraMitmLoggingMixin
   @override
   Future<void> connect({String host = 'localhost', int port = 8082}) async {
     log.info('Connecting to HTTP interception backend ($host:$port)...');
-    await super.connect(host: host, port: port);
+    try {
+      await super.connect(host: host, port: port);
+    } on IOException catch (e) {
+      log.severe(
+        'Failed to connect to HTTP interception backend ($host:$port): $e',
+      );
+      rethrow;
+    }
     log.info('Connected to HTTP interception backend ($host:$port).');
     done.then((_) => log.info('Disconnected from HTTP interception backend.'));
   }
