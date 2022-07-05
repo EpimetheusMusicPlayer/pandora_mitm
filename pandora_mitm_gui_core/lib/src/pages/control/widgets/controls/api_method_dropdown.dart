d2be72fc90574e53a4d3558f699bb138ad8748c7
hacker1024 <hacker1024@users.sourceforge.net>
2022-07-05 19:06:56 +1000

    feat(gui): Implement API method selection dropdown widget

---
+++ pandora_mitm_gui_core/lib/src/pages/control/widgets/controls/api_method_dropdown.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pandora_mitm_gui_core/src/state/pandora_mitm_bloc.dart';
+
+class ApiMethodDropdown extends StatelessWidget {
+  final Set<String> apiMethods;
+  final bool selectionValid;
+  final TextStyle? textStyle;
+
+  const ApiMethodDropdown({
+    Key? key,
+    required this.apiMethods,
+    this.selectionValid = true,
+    this.textStyle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const hintText = 'API method';
+    return BlocSelector<PandoraMitmBloc, PandoraMitmState, String?>(
+      selector: (state) => state.requireConnected.selectedApiMethod,
+      builder: (context, selectedApiMethod) {
+        return DropdownButton<String>(
+          hint: _SelectedDropdownMenuItem(
+            child: Text(hintText, style: textStyle),
+          ),
+          disabledHint: _SelectedDropdownMenuItem(
+            child: Text(
+              hintText,
+              style:
+                  textStyle?.copyWith(color: textStyle?.color?.withAlpha(0xB2)),
+            ),
+          ),
+          value: selectionValid ? selectedApiMethod : null,
+          onChanged: (apiMethod) =>
+              context.read<PandoraMitmBloc>().selectApiMethod(apiMethod),
+          iconEnabledColor: textStyle?.color,
+          // focusColor:
+          //     Colors.transparent /* https://stackoverflow.com/q/71066871 */,
+          items: apiMethods
+              .map(
+                (apiMethod) => DropdownMenuItem(
+                  value: apiMethod,
+                  child: Text(apiMethod),
+                ),
+              )
+              .toList(growable: false),
+          selectedItemBuilder: (_) => apiMethods
+              .map(
+                (apiMethod) => _SelectedDropdownMenuItem(
+                  child: Text(apiMethod, style: textStyle),
+                ),
+              )
+              .toList(growable: false),
+        );
+      },
+    );
+  }
+}
+
+class _SelectedDropdownMenuItem extends StatelessWidget {
+  final Widget child;
+
+  const _SelectedDropdownMenuItem({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownMenuItem(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8),
+        child: child,
+      ),
+    );
+  }
+}
